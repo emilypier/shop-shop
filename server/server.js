bbb49723c92a917d42f705c2ba5f81bd15f73a32
emilypier <piersoee@miamioh.edu>
2022-04-02 11:21:48 -0400

    deploying

--- server/server.js
+++ server/server.js
@@ -14,10 +14,10 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shop-shop-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shop-shop-db', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 server.applyMiddleware({ app });
 
